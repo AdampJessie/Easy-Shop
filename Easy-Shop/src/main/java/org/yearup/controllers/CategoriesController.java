eadae403927dd448bd791f88a63347eca93d1610
unknown <adampjessie@gmail.com>
2025-06-23 20:14:47 -0400

    Removed comments, added necessary SpringBoot annotations, completed CategoriesController

--- Easy-Shop/src/main/java/org/yearup/controllers/CategoriesController.java
+++ Easy-Shop/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -8,21 +9,25 @@ import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
-
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+@RestController
+@RequestMapping("products")
+@CrossOrigin
+
 public class CategoriesController {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
+    @Autowired
+    public CategoriesController(ProductDao productDao, CategoryDao categoryDao)
+    {
+        this.productDao = productDao;
+        this.categoryDao = categoryDao;
+    }
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
-
-    // add the appropriate annotation for a get action
+    @GetMapping("")
+    @PreAuthorize("permitAll()")
     public List<Category> getAll() {
         try
         {
@@ -34,7 +39,8 @@ public class CategoriesController {
         }
     }
 
-    // add the appropriate annotation for a get action
+    @GetMapping("")
+    @PreAuthorize("permitAll()")
     public Category getById(@PathVariable int id) {
         Category category = null;
         try
@@ -54,8 +60,6 @@ public class CategoriesController {
         return category;
     }
 
-    // the url to return all products in category 1 would look like this
-    // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
     @PreAuthorize("permitAll()")
     public List<Product> getProductsById(@PathVariable int categoryId) {
@@ -66,8 +70,6 @@ public class CategoriesController {
         }
     }
 
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
     @PostMapping()
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ResponseStatus(value = HttpStatus.CREATED)
@@ -79,8 +81,6 @@ public class CategoriesController {
         }
     }
 
-    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
     @PutMapping("{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category) {
@@ -91,9 +91,6 @@ public class CategoriesController {
         }
     }
 
-
-    // add annotation to call this method for a DELETE action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
     @DeleteMapping("{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
